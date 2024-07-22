74f1cfddd7c8e216ae03c76ea09730ec2d51817d
nicagali <m.conte@uu.nl>
2024-07-22 14:07:43 +0200

    Exact expr g_infty. rhob parameter

---
+++ ahkab_old1/components/Mysistor.py
@@ -0,0 +1,124 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright 2006 Giuseppe Venturini
+
+# This file is part of the ahkab simulator.
+#
+# Ahkab is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# Ahkab is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License v2
+# along with ahkab.  If not, see <http://www.gnu.org/licenses/>.
+from .Component import Component
+
+import numpy as np
+
+class Mysistor(Component):
+    """A memristor.
+
+    .. image:: images/elem/resistor.svg
+
+    **Parameters:**
+
+    part_id : string
+        The unique identifier of this element. The first letter should be
+        ``'R'``.
+    n1 : int
+        *Internal* node to be connected to the anode.
+    n2 : int
+        *Internal* node to be connected to the cathode.
+    value : float
+        Resistance in ohms.
+
+     """
+    #
+    #             /\    /\    /\
+    #     n1 o---+  \  /  \  /  \  +---o n2
+    #                \/    \/    \/
+    #
+    def __init__(self, part_id, n1, n2, value, rho_b, tau=0.0048):
+        self.part_id = part_id
+        self._value = value
+        self._g = 1./value
+        self.is_nonlinear = False
+        self.is_symbolic = True
+        self.n1 = n1
+        self.n2 = n2
+        
+        self.tau = tau
+        self.rho_b = rho_b
+        
+        self.length_channel = 10e-6
+        self.radius_base = 200e-9
+        self.radius_tip = 50e-9
+        self.delta_radius = self.radius_base - self.radius_tip
+        self.electron_charge = 1.60217663e-19
+        self.boltzman_const = 1.38e-23
+        self.temperature = 293.15
+        self.peclet_number = 16.5
+        
+        self.delta_g = -2*(9.5)*self.delta_radius*0.025/(self.radius_base*self.rho_b)
+        # print(self.delta_g)
+        
+        Dvalue = 1.75e-9
+        avogadro_number = 6.022e23
+        g_1 = np.pi*self.radius_tip*self.radius_base/self.length_channel
+        g_2 = 2*self.rho_b*(self.electron_charge**2)*Dvalue/(self.boltzman_const*self.temperature)
+        
+        self.g_0 = avogadro_number*g_1*g_2*1e12
+        # print(self.g_0)
+
+
+    @property
+    def g(self, v=0, time=0):
+        return self._g
+
+    @g.setter
+    def g(self, g):
+        self._g = g
+        self._value = 1./g
+
+    @property
+    def value(self, v=0, time=0):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+        self._g = 1./value
+
+    def new_nodes(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def i(self, v, time=0):
+        return 0
+
+    def get_op_info(self, ports_v):
+        """Information regarding the Operating Point (OP)
+
+        **Parameters:**
+
+        ports_v : list of lists
+            The parameter is to be set to ``[[v]]``, where ``v`` is the voltage
+            applied to the resistor terminals.
+
+        **Returns:**
+
+        op_keys : list of strings
+            The labels corresponding to the numeric values in ``op_info``.
+        op_info : list of floats
+            The values corresponding to ``op_keys``.
+        """
+        vn1n2 = float(ports_v[0][0])
+        in1n2 = float(ports_v[0][0]/self.value)
+        power = float(ports_v[0][0] ** 2 / self.value)
+        
+        op_keys = ['Part ID', u"R [\u2126]", "V(n1,n2) [V]", "I(n1->n2) [A]", "P [W]"]
+        op_info = [self.part_id.upper(), self.value, vn1n2, in1n2, power]
+        return op_keys, op_info
---
+++ ahkab_old1/plotting_functions.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# size
+axis_fontsize = 17
+legend_size = 15
+size_ticks = 13
+size_labels = 18 
+
+output_voltage_style = dict(c = 'darkolivegreen', lw=2, label=r'$V_{O}$')
+input_voltage_style = dict(c = 'limegreen', lw=2, label=r'$V_{in}$')
+condct_time_style = dict(c = 'navy', lw=2, label=r'$g(t)$')
+current_res_style = dict(c = 'darkolivegreen', lw=2, label=r'$I_r$')
+current_written_style = dict(c = 'cadetblue', lw=2, label=r'$I_w$')
+
+def plot_voltages(ax, result, plot_type):
+
+    if plot_type=="output_voltage":
+
+        ax.plot(result['tran']['T'], result['tran']['VN2'], **output_voltage_style)
+
+        # print(result['tran']['T'], result['tran']['VN2'])
+
+    if plot_type=="input_voltage":
+
+        ax.plot(result['tran']['T'], result['tran']['VN1'], **input_voltage_style)
+
+
+    ax.grid(ls=':')
+    # ax.set_xlabel(r't[ms]', fontsize = axis_fontsize)
+
+    ax.tick_params('y', labelsize=size_ticks)
+    ax.tick_params('x', labelsize=size_ticks)
+    ax.legend()
+
+
+def plot_conductance(ax, result, plot_type):
+    
+
+    if plot_type=="analytical_sol":
+
+        data = np.loadtxt(f"../data/solution_euler_g.txt", unpack=True)
+        time = data[0]  
+        conductance = data[1]
+
+        ax.plot(time, conductance, **condct_time_style)
+
+    if plot_type=="time_dep":
+
+        conductance = result['tran']['I(V1)']/result['tran']['VN1']
+        print(1/conductance)
+        # print("-----------")
+        # print(conductance[0], conductance[1])
+        # print("-----------")
+        
+        ax.plot(result['tran']['T'], conductance, **condct_time_style)
+        # ax.plot(x_interval, conductance, **input_voltage_style)
+        # ax.plot(x_interval, y, **condct_time_style)
+
+
+    if plot_type=="volt_dep":
+
+        ax.plot(result['tran']['VN1'], conductance, **input_voltage_style)
+
+    ax.grid(ls=':')
+    # ax.set_ylim([0.01, 0.03])
+    # ax.set_xlabel(r't[ms]', fontsize = axis_fontsize)
+
+    ax.tick_params('y', labelsize=size_ticks)
+    ax.tick_params('x', labelsize=size_ticks)
+    ax.legend()
+
+
+def plot_current(ax, result):
+    
+    
+    ax.plot(result['tran']['T'], result['tran']['I(V1)'], **current_res_style)
+
+    data = np.loadtxt(f"../data/current_memr.txt", unpack=True)
+    current = data[1]
+    time=data[0]
+
+    ax.plot(time, current, **current_written_style)
+
+    ax.grid(ls=':')
+
+    ax.tick_params('y', labelsize=size_ticks)
+    ax.tick_params('x', labelsize=size_ticks)
+    ax.legend()
